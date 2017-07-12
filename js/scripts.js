7acb12827e1008e854edbff8dc527c6d86ea5bc5
Kimberly Lu <kimlu4@comcast.net>
2017-07-12 08:11:40 -0700

    add js and html to hide vowels in word puzzle and then reveal answer.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,37 @@ $(document).ready(function() {
   $("#word").submit(function(event) {
     event.preventDefault();
     var string = $("input#sentence").val().toLowerCase();
-    var results = [];
-    for (var i = 0; i < string.length; i++) {
-      if (string[i] === "i") {
-        results += string[i].replace(string[i], "-");
+    var pieces = string.split("");
+    var currentLetter = "";
+    var results = "";
+
+    for (var i = 0; i < pieces.length; i++) {
+      currentLetter = pieces[i];
+      if (currentLetter === "a") {
+        currentLetter = "-";
+      } else if (currentLetter === "e") {
+        currentLetter = "-";
+      } else if (currentLetter === "i") {
+        currentLetter = "-";
+      } else if (currentLetter === "o") {
+        currentLetter = "-";
+      } else if (currentLetter === "u") {
+        currentLetter = "-";
       } else {
-        results += string[i];
+        currentLetter = currentLetter;
       }
+      results = results + currentLetter;
     };
+
     $("#quiz").text(results);
     $("#puzzle").show();
     $("#word").hide();
   });
+
+  $("#click").click(function(event) {
+    var stringInitial = $("input#sentence").val();
+    $("#original").text(stringInitial);
+    $("#answer").show();
+  });
+
 });
